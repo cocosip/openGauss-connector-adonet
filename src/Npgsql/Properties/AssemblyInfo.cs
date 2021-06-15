c932edc0c964d88740c7931b51d38507ecc113e7
rainrcn <rainrcn@qq.com>
2021-06-15 19:54:46 +0800

    移植Npgsql-5.0.7版本的Ado.Net Connector

---
+++ src/Npgsql/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Security;
+
+[assembly: CLSCompliant(false)]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: AssemblyTrademark("")]
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
